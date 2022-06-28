f93db2b0105c85d0a61cccf046488b2bcdeb82c2
shengshengyang <97731209+shengshengyang@users.noreply.github.com>
2022-06-28 19:00:40 +0800

    現行版本上傳

--- demo-backend/src/main/java/com/eeit45/champion/vegetarian/VegetarianApplication.java
+++ demo-backend/src/main/java/com/eeit45/champion/vegetarian/VegetarianApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@ImportResource("classpath:payment_conf.xml")
 public class VegetarianApplication {
 
     public static void main(String[] args) {
