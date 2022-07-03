14e45378b5d64964d15cf8e493f976e9eb7f9db0
shengshengyang <97731209+shengshengyang@users.noreply.github.com>
2022-07-03 23:43:48 +0800

    刪除各種方法

--- demo-backend/src/main/java/com/eeit45/champion/vegetarian/service/shopCart/OrderService.java
+++ demo-backend/src/main/java/com/eeit45/champion/vegetarian/service/shopCart/OrderService.java
@@ -2,14 +2,11 @@ package com.eeit45.champion.vegetarian.service.shopCart;
 
 import com.eeit45.champion.vegetarian.dto.shopCart.CreateOrderRequest;
 import com.eeit45.champion.vegetarian.dto.shopCart.OrderQueryParams;
-import com.eeit45.champion.vegetarian.dto.shopCart.OrderRequest;
 import com.eeit45.champion.vegetarian.model.shopCart.Order;
 
 import java.util.List;
 
 public interface OrderService {
-
-    Integer createOrder(OrderRequest orderRequest);
 
     Integer createOrders(Integer userId, CreateOrderRequest createOrderRequest);
 
@@ -18,23 +15,7 @@ public interface OrderService {
 
     Integer countOrder(OrderQueryParams orderQueryParams);
 
-    Order getOrderById(Integer orderId);
-
     Order getOrdersById(Integer orderId);
-
-    public List<Order> getOrderByUserId(Integer userId);
-
-     void insertCartEntryIntoOrderEntry(String cartUUID);
-
-    void updateOrder(Integer orderId, OrderRequest orderRequest);
-
-    void updateOrderPayment(String payment,Integer orderId);
-
-    void updateOrderShipping(String shipping,Integer orderId);
-
-    List<Order> getAllOrder();
-
-    void deleteOrderById(Integer orderId);
 
 
 }
--- demo-backend/src/main/java/com/eeit45/champion/vegetarian/service/shopCart/impl/OrderServiceImpl.java
+++ demo-backend/src/main/java/com/eeit45/champion/vegetarian/service/shopCart/impl/OrderServiceImpl.java
@@ -5,7 +5,6 @@ import com.eeit45.champion.vegetarian.dao.shopCart.ProductDao;
 import com.eeit45.champion.vegetarian.dto.shopCart.BuyItem;
 import com.eeit45.champion.vegetarian.dto.shopCart.CreateOrderRequest;
 import com.eeit45.champion.vegetarian.dto.shopCart.OrderQueryParams;
-import com.eeit45.champion.vegetarian.dto.shopCart.OrderRequest;
 import com.eeit45.champion.vegetarian.model.shopCart.Order;
 import com.eeit45.champion.vegetarian.model.shopCart.OrderItem;
 import com.eeit45.champion.vegetarian.model.shopCart.Product;
@@ -35,11 +34,6 @@ public class OrderServiceImpl implements OrderService {
     //@Autowired
     //private UserDao userDao;
 
-    @Override
-    public Integer createOrder(OrderRequest orderRequest) {
-        return orderDao.createOrder(orderRequest);
-    }
-
 
     @Transactional // 讓兩個table的訂單都是同時新增成功 or 同時新增失敗的狀況
     @Override
@@ -55,12 +49,12 @@ public class OrderServiceImpl implements OrderService {
 //        }
 
 
-        Integer totalAmount = 0 ;
+        Integer totalAmount = 0;
 
         List<OrderItem> orderItemList = new ArrayList<>();
 
         // for Loop 整個List 裡面的 Product ， 去把值一一抓出去做+-*/
-        for(BuyItem buyItem : createOrderRequest.getBuyItemList()){
+        for (BuyItem buyItem : createOrderRequest.getBuyItemList()) {
             Product product = productDao.getProductById(buyItem.getProductId());
 
 
@@ -68,8 +62,8 @@ public class OrderServiceImpl implements OrderService {
             //會由此去判斷庫存是否足夠，若不夠的話則拒絕創建訂單。
             //前端若有調整庫存數據的code，應為送一筆[ { productId : stock } ,{ productId : stock }]
             //針對product欄位去調整庫存，且令product的stock欄位為負值
-            if(product == null ){
-                log.warn("商品{}不存在",buyItem.getProductId());
+            if (product == null) {
+                log.warn("商品{}不存在", buyItem.getProductId());
                 throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
             }
 //            else if (product.getStock() < buyItem.getQuantity()){
@@ -95,10 +89,10 @@ public class OrderServiceImpl implements OrderService {
 
         //Created Order
         //在資料庫中，訂單是由兩個table所創立出來的，所以也要call兩個不同的table
-        Integer orderId = orderDao.createOrders(userId,totalAmount); //在order table 創建一筆資料出來
+        Integer orderId = orderDao.createOrders(userId, totalAmount); //在order table 創建一筆資料出來
 
         //在 order_item table 創立另一筆資料出來
-        orderDao.createOrderItems(orderId,orderItemList);
+        orderDao.createOrderItems(orderId, orderItemList);
 
         return orderId;
     }
@@ -108,7 +102,7 @@ public class OrderServiceImpl implements OrderService {
 
         List<Order> orderList = orderDao.getOrders(orderQueryParams);
 
-        for (Order order : orderList){
+        for (Order order : orderList) {
             List<OrderItem> orderItemList = orderDao.getOrderItemsByOrderId(order.getOrderId());
 
             order.setOrderItemList(orderItemList);
@@ -122,10 +116,6 @@ public class OrderServiceImpl implements OrderService {
         return orderDao.countOrder(orderQueryParams);
     }
 
-    @Override
-    public Order getOrderById(Integer orderId) {
-        return orderDao.getOrderById(orderId);
-    }
 
     @Override
     public Order getOrdersById(Integer orderId) {
@@ -138,39 +128,6 @@ public class OrderServiceImpl implements OrderService {
         return order;
     }
 
-    @Override
-    public List<Order> getOrderByUserId(Integer userId) {
-        return orderDao.getOrderByUserId(userId);
-    }
-
-    @Override
-    public void insertCartEntryIntoOrderEntry(String cartUUID) {
-        orderDao.insertCartEntryIntoOrderEntry(cartUUID);
-    }
-
-    @Override
-    public void updateOrder(Integer orderId, OrderRequest orderRequest) {
-        orderDao.updateOrder(orderId,orderRequest);
-    }
-
-    @Override
-    public void updateOrderPayment(String payment, Integer orderId) {
-        orderDao.updateOrderPayment(payment,orderId);
-    }
-    @Override
-    public void updateOrderShipping(String shipping, Integer orderId) {
-        orderDao.updateOrderShipping(shipping,orderId);
-    }
-
-    @Override
-    public List<Order> getAllOrder() {
-        return orderDao.getAllOrder();
-    }
-
-    @Override
-    public void deleteOrderById(Integer orderId) {
-        orderDao.deleteOrderById(orderId);
-    }
 }
 
 
