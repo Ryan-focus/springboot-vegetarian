72c8ab07580b479b8fa3cdbd0f1f1abce250f830
Raven <ryan.t18910@gmail.com>
2022-07-06 18:04:23 +0800

    後台顯示 pos List 方法測試完畢

--- demo-backend/src/main/java/com/eeit45/champion/vegetarian/dto/customer/PosRequest.java
+++ demo-backend/src/main/java/com/eeit45/champion/vegetarian/dto/customer/PosRequest.java
@@ -3,26 +3,16 @@ package com.eeit45.champion.vegetarian.dto.customer;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.util.Date;
-import java.util.List;
+
 
 public class PosRequest {
 
     //創建一個參數對應前端送的資料
 
-    @NotNull
-    private Integer businessId;
-    @NotNull
+    @NotEmpty
     private String validDate;
     @NotNull
-    private Date expiryDate;
-
-    public Integer getBusinessId() {
-        return businessId;
-    }
-
-    public void setBusinessId(Integer businessId) {
-        this.businessId = businessId;
-    }
+    private String expiryDate;
 
     public String getValidDate() {
         return validDate;
@@ -32,11 +22,11 @@ public class PosRequest {
         this.validDate = validDate;
     }
 
-    public Date getExpiryDate() {
+    public String getExpiryDate() {
         return expiryDate;
     }
 
-    public void setExpiryDate(Date expiryDate) {
+    public void setExpiryDate(String expiryDate) {
         this.expiryDate = expiryDate;
     }
 }
