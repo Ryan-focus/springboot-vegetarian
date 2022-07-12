35ba40554ab1e8f2523bc81a8982ae2c683280a2
shengshengyang <97731209+shengshengyang@users.noreply.github.com>
2022-07-13 07:26:24 +0800

    按完結帳鈕之後跳轉頁面

--- demo-frontend/src/views/frontend/CheckOut.vue
+++ demo-frontend/src/views/frontend/CheckOut.vue
@@ -2,12 +2,16 @@
 import { ref } from "vue";
 import axios from "axios";
 const sum = ref()
+const url = ref()
 function payment() {
     axios.post(
         "http://localhost:8088/paypal/payment?sum=" + sum.value
     ).then(
         (res) => {
-            console.log(res)
+            // 抓出返回值得跳轉頁面網址
+            if (res.data.redirect_url != null) {
+                window.location = res.data.redirect_url
+            }
         }
     )
 }
