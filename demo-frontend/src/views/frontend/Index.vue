02a73f7555029e77a64e0ff3210ee7ea9760ac81
love0915i <love0915i@gmail.com>
2022-07-22 15:48:17 +0800

    首頁app圖拿掉

--- demo-frontend/src/views/frontend/Index.vue
+++ demo-frontend/src/views/frontend/Index.vue
@@ -63,16 +63,17 @@ function searchAddress(restaurantAddress) {
   <div>
     <!-- Hero -->
     <div id="one-vue-hero" class="bg-body-extra-light">
-      <BaseBackground image="/assets/media/vegan/index-bg.jpg" inner-class="bg-black-25">
+      <BaseBackground image="/assets/media/vegan/index-bg.jpg" inner-class="bg-black-50">
         <div class="content content-full text-center">
-          <div class="my-1"></div>
-          <h1 class="h1 text-white mb-3"><b>想知道哪裡有素食 ?</b></h1>
+          <!-- <div class="my-1"></div> -->
+          <h1 class="h1 text-white mb-3" style="margin-top:3.5cm"><b>想知道哪裡有素食 ?</b></h1>
           <h3>
-            <span class="text-white-75"><b>從超過 50 家精選餐廳中，探索您不知道的熱門素食。</b></span>
+            <span class="text-white-75" style="margin-top:1cm"><b>從超過 50
+                家精選餐廳中，探索您不知道的熱門素食。</b></span>
           </h3>
         </div>
         <form @submit.prevent>
-          <div class="row">
+          <div class=" row">
             <div class="col-md-5 offset-md-3 content content-full text-center">
               <div class="mb-2">
                 <div>
@@ -107,26 +108,14 @@ function searchAddress(restaurantAddress) {
             </div>
           </div>
         </form>
-        <div class="row">
-          <div class="col-md-5 offset-md-4 justify-content-center in row">
-            <div class="col-lg-4 mb-3 me-auto">
-              <a href="">
-                <img src="https://ifoodie.tw/assets/images/app_store_badge.png" alt="" class="img-fluid" /></a>
-            </div>
-            <div class="col-lg-4 mb-3 me-auto">
-              <a href=""><img src="https://play.google.com/intl/en_us/badges/images/apps/zh-tw-play-badge.png" alt=""
-                  class="img-fluid" /></a>
-            </div>
-          </div>
-        </div>
       </BaseBackground>
     </div>
     <!-- END Hero -->
 
     <!-- Hero After -->
-    <div id="one-vue-hero-after" class="bg-body-light">
+    <!-- <div id="one-vue-hero-after" class="bg-body-light">
       <div class="content content-full"></div>
-    </div>
+    </div> -->
     <!-- END Hero After -->
 
 
@@ -135,10 +124,7 @@ function searchAddress(restaurantAddress) {
       <div class="content py-5">
         <div class="row fs-sm fw-medium">
           <div class="col-sm-6 order-sm-2 py-1 text-center text-sm-end">
-            <!-- Crafted with -->
-            <!-- <i class="fa fa-heart text-danger"></i> by -->
-            <!-- <a class="fw-semibold" href="https://1.envato.market/ydb"
-              ></a -->
+
           </div>
           <div class="col-sm-6 order-sm-1 py-1 text-center text-sm-start">
             <a class="fw-semibold" href="/">{{
